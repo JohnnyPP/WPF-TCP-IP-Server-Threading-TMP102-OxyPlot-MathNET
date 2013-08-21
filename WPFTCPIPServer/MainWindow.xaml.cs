9d766676381e78a4694799e9dcd5c75d70b2fb7a
JohnnyPP <johnnnyppp@gmail.com>
2013-08-21 22:18:13 +0200

    Initial release

---
+++ WPFTCPIPServer/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTCPIPServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private System.Diagnostics.Stopwatch stopwatch = new Stopwatch();
+        private long lastUpdateMilliSeconds;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+            stopwatch.Start();
+        }
+
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            if (stopwatch.ElapsedMilliseconds > lastUpdateMilliSeconds + 1000)
+            {
+                plot1.RefreshPlot(true);
+                lastUpdateMilliSeconds = stopwatch.ElapsedMilliseconds;
+                //Without this 1000 ms delay after some time the program throws exception: 
+                //"Collection was modified; enumeration operation may not execute"
+                //It is probably due to too fast plot1 refresh rate.
+                //In general this delay should be correlated with the client data sending rate.
+            }
+        }
+    }
+}
---
+++ WPFTCPIPServer/ViewModel.cs
@@ -0,0 +1,153 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Input;
+using WpfApplicationPropertyChanged;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using MathNet.Numerics.Statistics;
+
+namespace WPFTCPIPServer
+{
+
+    [ImplementPropertyChanged]
+    public class ViewModel
+    {
+        int x = 0;
+
+        public string Temperature { get; set; }
+        public string ServerStatus { get; set; }
+
+        private List<double> TemperatureList = new List<double>();
+
+        public Collection<CollectionDataValue> Data { get; set; }
+
+        public class CollectionDataValue
+        {
+            public double xData { get; set; }
+            public double yData { get; set; }
+        }
+
+        public ViewModel()
+        {
+            ServerStatus = "Press Start server button to start the TCP/IP server";
+            Data = new Collection<CollectionDataValue>();
+        }
+
+        void UpdateControlExecute()
+        {
+            Thread newThread = new Thread(new ThreadStart(StartServer));
+            newThread.Start();
+        }
+
+        private void StartServer()
+        {
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("192.168.2.101");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop. 
+                while (true)
+                {
+                    ServerStatus = "Waiting for a connection... ";
+
+                    // Perform a blocking call to accept requests. 
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    ServerStatus = "Connected!";
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client. 
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        string TemperatureString;
+                        double TemperatureDouble;
+                        DescriptiveStatistics descrStat = new DescriptiveStatistics(TemperatureList);
+                        double dKurtosis = descrStat.Kurtosis;
+                        double dSkewness = descrStat.Skewness;
+
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        TemperatureString = data;
+                        TemperatureDouble = double.Parse(TemperatureString, NumberStyles.Float, CultureInfo.InvariantCulture);
+
+                        
+
+                        Data.Add(new CollectionDataValue { xData = x, yData = TemperatureDouble });
+                        TemperatureList.Add(TemperatureDouble);
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        //Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        
+                        ServerStatus = "Receiving temperature data and sending response to the Client.";
+
+                        #region TemperatureStatistics
+                        Temperature = "Current temperature: " + String.Format("{0:0.0000}", TemperatureDouble) + " [°C]" + Environment.NewLine +
+                        "Mean: " + String.Format("{0:0.0000}", TemperatureList.Mean()) + " [°C]" + Environment.NewLine +
+                        "Median: " + String.Format("{0:0.0000}", TemperatureList.Median()) + " [°C]" + Environment.NewLine +
+                        "Standard deviation: " + String.Format("{0:0.0000}", TemperatureList.StandardDeviation()) + " [°C]" + Environment.NewLine +
+                        "3x Standard deviation: " + String.Format("{0:0.0000}", 3 * TemperatureList.StandardDeviation()) + " [°C]" + Environment.NewLine +
+                        "Max: " + String.Format("{0:0.0000}", TemperatureList.Max()) + " [°C]" + Environment.NewLine +
+                        "Min: " + String.Format("{0:0.0000}", TemperatureList.Min()) + " [°C]" + Environment.NewLine +
+                        "Kurtosis: " + String.Format("{0:0.0000}", descrStat.Kurtosis) + "\r\n" +
+                        "Skewness: " + String.Format("{0:0.0000}", descrStat.Skewness) + "\r\n" +
+                        "Sample number: " + Convert.ToString(x);
+                        #endregion
+                        
+                        x++;  
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Debug.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+        }
+
+        bool CanUpdateControlExecute()
+        {
+            return true;
+        }
+
+        public ICommand StartServerButton
+        {
+            get { return new RelayCommand(UpdateControlExecute, CanUpdateControlExecute); }
+        }
+    }
+}
